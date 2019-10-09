75a8c22ac1aaef52169bf0c4bdf1dbb40eb158af
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-09 16:21:00 +0300

    Added connectionString and class for data

---
+++ AdminkaAPI1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AdminkaAPI1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AdminkaAPI1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection") { }
+        public DbSet<Entry> Entries { get; set; }
+
+    }
+}
--- AdminkaAPI1/Models/Entry.cs
+++ AdminkaAPI1/Models/Entry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace AdminkaAPI1.Models
 {
     public class Entry
     {
+        [Key]
+        public int Id { get; set; }
         public string Description { get; set; }
         public bool IsExpense { get; set; }
         public double Value { get; set; }
