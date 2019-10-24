eac3a2db8ab3c7fc73d7b03f46703449c68bf90e
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-24 20:45:31 +0300

    Created CreateToken() method

--- AdminkaAPI1/Controllers/AuthenticationController.cs
+++ AdminkaAPI1/Controllers/AuthenticationController.cs
@@ -1,9 +1,13 @@
 ﻿using AdminkaAPI1.Models;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
 using System.Web.Http;
 using System.Web.Http.Cors;
 
@@ -29,7 +33,39 @@ namespace AdminkaAPI1.Controllers
             return null;
         }
 
+        private JwtPackage CreateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var claims = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Email, user.UserName)
+            });
 
+            const string secretKey = "your secret key goes here";
+            var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(secretKey));
+            var signinCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = (JwtSecurityToken)tokenHandler.CreateJwtSecurityToken(
+                subject: claims,
+                signingCredentials: signinCredentials
+                );
+
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return new JwtPackage()
+            {
+                UserName = user.UserName,
+                Token = tokenString
+            };
+        }
 
     }
 }
+
+public class JwtPackage
+{
+    public string Token { get; set; }
+    public string UserName { get; set; }
+
+}
+
+
